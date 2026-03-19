06dccd7f63e891d770ccc46b152a4b720f3260dc
ELUCKTRON <saeedkhanloo@yahoo.com>
2026-03-19 23:37:21 +0100

    placing data for test

--- client/src/data/aboutData.ts
+++ client/src/data/aboutData.ts
@@ -2,21 +2,12 @@ import type { AboutPerson } from "../types";
 
 export const aboutData: AboutPerson[] = [
   {
-    title: "Assist. Prof.",
-    name: "Gianluca",
-    familyName: "Bonifazi",
-    job: "Visiting Professor from Università Politecnica delle Marche, Italy",
-    extraInfo: "Host Professors: Prof. Ezio Bartocci, Prof. Katjia Hose",
-    picture: "/avatars/gianluca-bonifazi.png",
-    link: "https://www.linkedin.com/",
-  },
-  {
   title: "Dr.",
   name: "Ali",
   familyName: "Alshawki",
   job: "Researcher and contributor",
   extraInfo: "Connected to ELTE Cybersecurity and related academic or technical activities.",
-  picture: "/avatars/alshawki.png",
+  picture: "avatars/alshawki.jpg",
   link: "https://www.linkedin.com/in/alshawki/",
 }
   ,
@@ -27,7 +18,16 @@ export const aboutData: AboutPerson[] = [
     job: "Frontend Developer and Web Portal Contributor",
     extraInfo:
       "Working on the ELTE Cybersecurity portal for project presentation, resources, and future content.",
-    picture: "/avatars/saeed.png",
+    picture: "avatars/saeedkhanloo.jpg",
     link: "https://www.linkedin.com/in/saeedkhanloo",
+  },
+    {
+    title: "Assist. Prof.",
+    name: "Gianluca",
+    familyName: "Bonifazi",
+    job: "Visiting Professor from Università Politecnica delle Marche, Italy",
+    extraInfo: "Test Person",
+    picture: "avatars/gianluca-bonifazi.png",
+    link: "https://www.linkedin.com/",
   },
 ];
